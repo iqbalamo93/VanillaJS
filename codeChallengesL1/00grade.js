30cce286958a836adf239e4660979a7a48dc77ad
Iqbal Singh <iqbalamo93@gmail.com>
2022-07-24 20:52:24 +0530

    'object'

--- codeChallengesL1/00grade.js
+++ codeChallengesL1/00grade.js
@@ -7,7 +7,7 @@ const finalGrade = (a,b,c) =>{
             if(avg>0 && avg <= 59){ return 'F'}
                 else if(avg>=60 && avg<=69) {return 'D'}
                 else if(avg>=70 && avg<=79) {return 'C'}
-                else if(avg>=80 && avg<=69) {return 'B'}
+                else if(avg>=80 && avg<=89) {return 'B'}
                 else if(avg>=90 && avg<=100) {return 'A'}
         }
 
