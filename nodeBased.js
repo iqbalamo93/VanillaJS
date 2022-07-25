f7cc4071964f20ba7cb74748e48f2dab87dacffa
Iqbal Singh <iqbalamo93@gmail.com>
2022-07-25 19:07:06 +0530

    's'

--- nodeBased.js
+++ nodeBased.js
@@ -1,1 +1,6 @@
-console.log(process.env.PWD);
+console.log(process.env.HOMEPATH);
+
+console.log(global);
+
+console.log('Now running process.env.......................');
+console.log(process.env);
