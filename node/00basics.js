90d771f56df9f5c0f579312949534156280d5d08
Iqbal Singh <iqbalamo93@gmail.com>
2022-07-25 20:33:42 +0530

    'NodeUpdate'

---
+++ node/00basics.js
@@ -0,0 +1,10 @@
+console.log(process.env.HOMEPATH);
+
+console.log(global);
+
+console.log('Now running process.env.......................');
+console.log(process.env);
+
+console.log('Now running MEMORY.......................');
+
+console.log( process.memoryUsage());
--- nodeBased.js
+++
@@ -1,6 +0,0 @@
-console.log(process.env.HOMEPATH);
-
-console.log(global);
-
-console.log('Now running process.env.......................');
-console.log(process.env);
